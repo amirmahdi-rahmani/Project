35ec89642aad86a20a8c62ec9579be210f6fe8ff
AmirMahdi <rahmani.a92m@gmail.com>
2023-11-30 11:51:54 +0330

    pushed by amir

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const azarbaijanData = {
+  name: "azarbaijan",
+  image: "some address",
+  centerCity: "tabriz",
+  sanayedasti: [
+    {
+      id: "100",
+      name: "farsh bafi",
+      images: [
+        { id: "201", value: "imageAddress" },
+        { id: "202", value: "imageAddress 2" },
+      ],
+    },
+  ],
+  amakendDidani: [
+    {
+      id: "300",
+      name: "elgoali",
+      images: [
+        { id: "401", value: "imageAddress" },
+        { id: "402", value: "imageAddress 2" },
+      ],
+    },
+  ],
+};
+
+export default azarbaijanData;
